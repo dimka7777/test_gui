269c76380e25ca97ede57a60df2cd7c1a63cab4b
dzman_msi_laptop <dzman@ya.ru>
2022-06-02 10:22:05 +0300

    Попытка заработать dearpygui

--- first_step.py
+++ first_step.py
@@ -1,1 +1,13 @@
-print("Hello! Friend!")
+import dearpygui as d
+
+
+def save_callback(sender, data):
+    print("Save Clicked")
+
+
+d.core.add_text("Hello, world")
+d.core.add_button("Save", callback=save_callback)
+d.core.add_input_text("string", default_value="Quick brown fox")
+d.core.add_slider_float("float", default_value=0.273, max_value=1)
+
+d.core.start_dearpygui()
